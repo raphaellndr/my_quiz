dcc93e7c727a662bca3fa94de8c2bf6e930f903c
Landuré Raphaël <raph.landure@gmail.com>
2020-05-29 18:39:31 +0200

    Questions corrections

--- lib/quiz_page.dart
+++ lib/quiz_page.dart
@@ -24,11 +24,11 @@ class _QuizPage extends State<QuizPage> {
     Question('La Russie est plus grande en superficie que Pluton', true, '', 'assets/russie.jpg'),
     Question('Nyctalope est une race naine d\'antilope', false, 'C’est une aptitude à voir dans le noir', 'assets/nyctalope.jpg'),
     Question('Le Commodore 64 est l\’oridnateur de bureau le plus vendu', true, '', 'assets/commodore.jpg'),
-    Question('Le nom du drapeau des pirates es black skull', false, 'Il est appelé Jolly Roger', 'assets/pirate.png'),
+    Question('Le nom du drapeau des pirates est Black Skull', false, 'Il est appelé Jolly Roger', 'assets/pirate.png'),
     Question('Haddock est le nom du chien Tintin', false, 'C\'est Milou', 'assets/tintin.jpg'),
     Question('La barbe des pharaons était fausse', true, 'A l\'époque déjà ils utilisaient des postiches', 'assets/pharaon.jpg'),
-    Question('Au Québec tire toi une bûche veut dire viens viens t\'asseoir', true, 'La bûche, fameuse chaise de bucheron', 'assets/buche.jpg'),
-    Question('Le module lunaire Eagle de possédait de 4Ko de Ram', true, 'Dire que je me plains avec mes 8GO de ram sur mon mac', 'assets/eagle.jpg'),
+    Question('Au Québec tire toi une bûche veut dire viens t\'asseoir', true, 'La bûche, fameuse chaise de bucheron', 'assets/buche.jpg'),
+    Question('Le module lunaire Eagle ne possédait que 4Ko de Ram', true, 'Dire que je me plains avec mes 8GO de ram sur mon mac', 'assets/eagle.jpg'),
   ];
 
   Question actualQuestion;
